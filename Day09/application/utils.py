483889ecfab7c396641c13a0450282785e5da9da
Simon-cf <1695251128@qq.com>
2020-12-16 12:08:58 +0800

    python深入浅出16天

---
+++ Day09/application/utils.py
@@ -0,0 +1,14 @@
+def create_http_response(status, response_body):
+    # 9、拼接响应的报文
+    # 9.1 响应行
+    response_line = "HTTP/1.1 %s\r\n" % status
+    # 9.2 响应头
+    response_header = "Server:Python20WS/2.1\r\n"
+    response_header += "Content-Type: text/html\r\n"
+    # 9.3 响应空行
+    response_blank = "\r\n"
+    # 9.4 响应的主体
+
+    response_data = (response_line + response_header + response_blank).encode() + response_body
+
+    return response_data
